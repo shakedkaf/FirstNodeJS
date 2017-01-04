31126a84ae8498e157b10eefff9baa0da7c65de8
shaked kafzan <shakedk@h-MacBook-Air-sl-shaked.local>
2017-01-04 20:56:26 +0200

    run

--- server2.js
+++ server2.js
@@ -3,6 +3,9 @@ var app = express();
 app.get('/', function (req, res) {
  res.send('Hello World!');
 });
-app.listen(3000, function () {
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function () {
  console.log('Example app listening on port 3000!');
 });
