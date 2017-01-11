efdd67413a375aaf788621c5a0429f928d0fc9a7
shaked kafzan <shakedk@h-MacBook-Air-sl-shaked.local>
2017-01-11 21:11:07 +0200

    get paramm 2

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
- res.send('Hello World!');
-});
 
 app.get('/', function (req, res) {
  var img = req.query.q;
