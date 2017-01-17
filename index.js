51a7e1cf9913036d1ee93efc9a8584eac6d7a682
shaked kafzan <shakedk@h-MacBook-Air-sl-shaked.local>
2017-01-17 17:57:23 +0200

    update index js print parmeters3

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+res.send('Hello connect to the server :)' );
+
+ // Get image name from the url
  var str_img = req.query.img;
+
+ // Check if get surf image
+ if (str_image === 'surf') {
+     // res.send('Get dwonlad surf image');
+     var img1 = fs.readFileSync('./logo.gif');
+     res.writeHead(200, {'Content-Type': 'images/surf' });
+     res.end(img1, 'binary');
+	// Open stream from surf file
+	//const readStream = fs.createReadStream('surf.jpeg');
+
+	
+ }
  res.send('Hello World! str_img = ' + str_img);
 });
 
