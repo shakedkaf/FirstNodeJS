66785a464d1c9befa73a8645c243a72c5fe9d297
shaked kafzan <shakedk@h-MacBook-Air-sl-shaked.local>
2017-01-18 20:51:28 +0200

    add pipe

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 
 var express = require('express');
+const fs = require('fs');
 var app = express();
 
 app.get('/', function (req, res) {
-//res.send('Hello connect to the server :)' );
 
- // Get image name from the url
- var str_img = req.query.img;
+// Get image name from the url
+var str_img = req.query.img;
 
- // Check if get surf image
- if (str_image === 'surf') {
-      res.send('Get dwonlad surf image');
-     var img1 = fs.readFileSync('./logo.gif');
-     res.writeHead(200, {'Content-Type': 'images/surf' });
-     res.end(img1, 'binary');
-	// Open stream from surf file
-	//const readStream = fs.createReadStream('surf.jpeg');
-
-	
- }
- res.send('Hello World! str_img = ' + str_img);
+// Check if get surf image
+if (str_image === 'surf') {
+	const readStream = fs.createReadStream('images/surf.jpeg');
+	readStream.pipe(res);	
+}
+ //res.send('Hello World! str_img = ' + str_img);
 });
 
 var port = process.env.PORT || 8080;
